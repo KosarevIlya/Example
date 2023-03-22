2f43b55380bb1fe466003d5891c94125603d7f9b
unknown <Kosarevis.74@gmail.com>
2023-03-22 11:38:46 +0500

    добавлен шестой элемент

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 4;
+int c = 6;
+int e = 8;
+int f = 1;
+
+int max = 0;
+ 
+ if (a > max) max = a;
+ if (b > max) max = b;
+ if (c > max) max = c;
+ if (e > max) max = e;
+ if (f > max) max = f;
+
+Console.Write ("max=");
+ Console.WriteLine (max);
